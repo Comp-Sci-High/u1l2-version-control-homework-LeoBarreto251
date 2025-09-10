4671914ea1e3382e074221167cb10ffb6a85c3b7
LeoBarreto251 <leandro.barreto26@compscihigh.org>
2025-09-10 21:47:56 +0000

    Answered What is git repo and the benefits of version control

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ console.log(versionControlExplanation);
 
 // 2. Explain what a Git repository is.
 // Write your answer as a string and store it in the variable below:
-let gitRepoExplanation = "";
+let gitRepoExplanation = "A git repository stores code in the repo which tracks code history";
 
 console.log("What is a Git repository?");
 console.log(gitRepoExplanation); 
 
 // 3. Name the benefits of using version control.
 // Write your answer as an array of strings below, with each string being a benefit.
-let versionControlBenefits = [];
+let versionControlBenefits = ["Helps to collaborate more effectively", "Allows you to revert to old versions of the code"];
 
 console.log("What are the benefits of using version control?");
 console.log(versionControlBenefits); 
